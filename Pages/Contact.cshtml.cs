e696d0f2700e6bd2c9beb30bb9dddc170a971ceb
Trong Tam <trongtam068@gmail.com>
2024-02-16 18:02:59 +0700

    Contact Form - Validation using Attributes and Model Binding

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace BestShop.Pages
 {
@@ -8,25 +10,52 @@ namespace BestShop.Pages
         public void OnGet()
         {
         }
+        [BindProperty]
+        [Required(ErrorMessage ="The First Name  is required")]
+        [Display(Name ="First Name*")]
         public string FirstName { get; set; } = "";
+
+		[BindProperty]
+		[Required(ErrorMessage = "The Last Name  is required")]
+        [Display(Name = "Last Name*")]
         public string LastName { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string Phone { get; set; } = "";
-        public string Subject { get; set; } = "";
+
+		[BindProperty]
+		[Required(ErrorMessage = "The Email is required")]
+        [EmailAddress]
+        [Display(Name ="Email*")]
+		public string Email { get; set; } = "";
+
+		[BindProperty]
+		public string Phone { get; set; } = "";
+
+		[BindProperty, Required]
+        [Display(Name = "Subject*")]
+		public string Subject { get; set; } = "";
+
+        public List<SelectListItem> SubjectList { get; } = new List<SelectListItem>
+        {
+            new SelectListItem {Value = "Order Status", Text = "Order Status"},
+            new SelectListItem {Value = "Refund Request", Text = "Refund Resquest"},
+            new SelectListItem {Value = "Job Application", Text = "Job Application"},
+            new SelectListItem {Value = "Other", Text = "Other"},
+        };
+
+		[BindProperty ]
+		[Required(ErrorMessage = "The Message is required")]
+        [MinLength(5, ErrorMessage ="The Message should be at least 5 characters")]
+        [MaxLength(1024, ErrorMessage = "The Message should be less than 1024 characters")]
+        [Display(Name ="Message*")]
         public string Message { get; set; } = "";
+
         public string SuccessMessage { get; set; } = "";
         public string ErrorMessage { get; set; } = "";  
     public void OnPost()
         {
-            FirstName = Request.Form["firstname"];
-            LastName = Request.Form["lastname"];
-            Email = Request.Form["email"];
-            Phone = Request.Form["phone"];
-            Subject = Request.Form["subject"];
-            Message = Request.Form["message"];
+            
 
             // check if any required field is empty 
-            if(FirstName.Length == 0 || LastName.Length == 0 || Email.Length == 0 || Subject.Length == 0 || Message.Length == 0)
+            if(!ModelState.IsValid)
             {
                 //error 
                 ErrorMessage = "Please fill all required fields";
@@ -34,6 +63,7 @@ namespace BestShop.Pages
             }
             // add this message to the database
             // send confirmation email tho the client 
+            if (Phone == null) Phone = "";
 
             SuccessMessage = "Your message has been received correctly";
             FirstName = "";
@@ -42,6 +72,8 @@ namespace BestShop.Pages
             Phone = "";
             Subject = "";
             Message = "";
+
+            ModelState.Clear();
         }
     }
 }
