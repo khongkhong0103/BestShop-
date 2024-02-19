68d3a9426c0dd35516a093a356cfb13090117b7f
Trong Tam <trongtam068@gmail.com>
2024-02-19 11:01:05 +0700

    Messages - Add Table Pagination

--- Pages/Admin/Messages/Index.cshtml.cs
+++ Pages/Admin/Messages/Index.cshtml.cs
@@ -7,17 +7,43 @@ namespace BestShop.Pages.Admin.Messages
     public class IndexModel : PageModel
     {
         public List<MessageInfo> listMeassages= new List<MessageInfo>();
+        public int page = 1; // the current html 
+        public int totalPages = 0;
+        private readonly int pageSize = 5; // each html page shows pageSize messages 
         public void OnGet()
         {
+            page = 1;
+            string requestPage = Request.Query["page"]; 
+            if(requestPage !=  null)
+            {
+                try
+                {
+                    page =int.Parse(requestPage);
+                }catch(Exception ex)
+                {
+                    page = 1;
+                }
+            }
             try
             {
                 string connectionString = "Data Source=DESKTOP-3CSJ4C0\\MSSQL;Initial Catalog=bestshop;Integrated Security=True";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
+                    string sqlCount = "SELECT COUNT(*) FROM messages";
+                    using (SqlCommand command = new SqlCommand(sqlCount, connection)) { 
+                                decimal count = (int)command.ExecuteScalar();
+                                totalPages = (int)Math.Ceiling(count/pageSize);
+                    };
+
                     string sql = "SELECT * FROM messages ORDER BY id DESC";
+                    sql += " OFFSET @skip ROWS FETCH NEXT @pageSize ROWS ONLY";
+
                     using(SqlCommand command = new SqlCommand(sql, connection))
                     {
+                        command.Parameters.AddWithValue("@skip", (page - 1 )* pageSize);
+                        command.Parameters.AddWithValue("@pageSize", pageSize);
+
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read()) { 
